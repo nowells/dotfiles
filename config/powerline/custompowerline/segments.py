4fb65169d8be9e57d345ae562de248d66ac8ddab
Nowell Strite <nowell@strite.org>
2013-01-29 16:19:40 -0500

    Added master branch support.

--- config/powerline/custompowerline/segments.py
+++ config/powerline/custompowerline/segments.py
@@ -13,3 +13,15 @@ def rvm():
         output, _ = proc.communicate()
         if output:
             return u'%s' % output.strip().decode('utf-8')
+
+
+def branch():
+    from powerline.lib.vcs import guess
+    repo = guess(os.path.abspath(os.getcwd()))
+    if repo:
+        branch = repo.branch()
+        return [{
+            'contents': branch,
+            'highlight_group': 'branch' if branch != 'master' else ['masterbranch', 'branch'],
+        }]
+    return None
