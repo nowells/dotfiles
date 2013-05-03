f6add0cc7723cdc1eff13432d7eb56a4c68bc62e
Nowell Strite <nowell@strite.org>
2013-05-03 10:39:00 -0400

    Updates.

--- config/js/mail.google.com.js
+++ config/js/mail.google.com.js
@@ -1,22 +1,61 @@
 function AddJiraLinks(node) {
     var JIRA_LINK_REGEX = /(\b[A-Z]{2,5}\-\d{1,5}\b)/g;
     var nodeText = node.nodeValue;
+    var caretPosition = GetCaretPosition();
     if (node.nodeType == 3 &&
         JIRA_LINK_REGEX.test(nodeText) &&
         node.parentNode !== null &&
         node.parentNode.nodeName !== 'A' &&
-        (node !== GetCaretPosition() && node.parentNode !== GetCaretPosition())
-       ) {
+        node.parentNode.className !== 'JIRALINK' &&
+        (node !== caretPosition && node.parentNode !== caretPosition)
+    ) {
         var jiraLink = document.createElement('span');
-        jiraLink.innerHTML = nodeText.replace(
-            JIRA_LINK_REGEX,
-            '<a ' +
-                'href="https://services.wisertogether.com/jira/browse/$1" ' +
-                'target="_blank">' +
-            '$1' +
-            '</a>'
-            );
-        node.parentNode.replaceChild(jiraLink, node);
+        jiraLink.className = 'JIRALINK';
+        var text = nodeText;
+
+        var matches = $.unique(nodeText.match(JIRA_LINK_REGEX));
+        var matched = 0;
+
+        for (var i = 0; i < matches.length; i++) {
+            var match = matches[i];
+
+            var updateLink = function(data, status) {
+                var issueKey = match,
+                    issueSummary = '',
+                    issueAssignee = '';
+
+                matched += 1;
+
+                if (status === 'success') {
+                    issueSummary = ' - ' + data.fields.summary;
+                    if (data.fields.assignee) {
+                        issueAssignee = ' (' + data.fields.assignee.displayName + ')'
+                    }
+                }
+
+                text = text.replace(
+                    match,
+                    '<a ' +
+                    'href="https://services.wisertogether.com/jira/browse/' + match + '"' +
+                    'target="_blank">' + match + '</a>' + issueSummary + issueAssignee
+                    );
+
+                if (matched === matches.length) {
+                    jiraLink.innerHTML = text;
+                    node.parentNode.replaceChild(jiraLink, node);
+                }
+                else {
+                    console.log(matched + ' : ' + matches.length);
+                }
+            };
+
+            $.ajax({
+                url: 'https://services.wisertogether.com/jira/rest/api/2/issue/' +
+                    match + '.json?fields=summary,assignee',
+                error: updateLink,
+                success: updateLink
+            });
+        }
     } else if (node.nodeType == 1) { // element
         $(node).contents().each(function() {
             AddJiraLinks(this);
@@ -26,6 +65,9 @@ function AddJiraLinks(node) {
 
 function GetCaretPosition() {
     var node = document.getSelection().anchorNode;
+    if (node === null) {
+        return;
+    }
     var startNode = (node.nodeType == 3 ? node.parentNode : node);
     return startNode;
 }
